2beb109fa9515b18f8ac7b995206e2e1ec36f590
BalazsArva <balazs.arva@windowslive.com>
2020-09-10 16:09:54 +0200

    Work on creating container regiatry.

---
+++ Terraform/acr.tf
@@ -0,0 +1,10 @@
+resource "azurerm_container_registry" "acr" {
+    name = "exampleapp"
+    resource_group_name = "LearningTerraform"
+    location = "North Europe"
+    sku = "standard"
+
+    tags = {
+        "environment": "development"
+    }
+}
