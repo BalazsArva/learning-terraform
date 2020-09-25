20d4797439eb2383c813ec999cddcc663c6e800f
BalazsArva <balazs.arva@windowslive.com>
2020-09-25 17:12:38 +0200

    Project for apiclient.

---
+++ App/Tests/LearningTerraform.Api.IntegrationTests/Controllers/OwnersControllerTests.cs
@@ -0,0 +1,19 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace LearningTerraform.Api.IntegrationTests.Controllers
+{
+    [TestFixture]
+    public class OwnersControllerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public async Task GetById_OwnerDoesNotExist_ReturnsNotFound()
+        {
+        }
+    }
+}
