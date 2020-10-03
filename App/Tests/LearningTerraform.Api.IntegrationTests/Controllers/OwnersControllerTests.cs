0affc0fa8695dd8d34980f7cd4cf96d8ee93226c
Árva Balázs <balazs.arva@windowslive.com>
2020-10-03 16:37:46 +0200

    Work on integration test skeleton.

--- App/Tests/LearningTerraform.Api.IntegrationTests/Controllers/OwnersControllerTests.cs
+++ App/Tests/LearningTerraform.Api.IntegrationTests/Controllers/OwnersControllerTests.cs
@@ -1,10 +1,12 @@
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
 using NUnit.Framework;
 
 namespace LearningTerraform.Api.IntegrationTests.Controllers
 {
     [TestFixture]
-    public class OwnersControllerTests
+    public class OwnersControllerTests : ControllerTestBase
     {
         [SetUp]
         public void SetUp()
@@ -14,6 +16,27 @@ namespace LearningTerraform.Api.IntegrationTests.Controllers
         [Test]
         public async Task GetById_OwnerDoesNotExist_ReturnsNotFound()
         {
+            var client = Factory.CreateClient();
+        }
+    }
+
+    public abstract class ControllerTestBase
+    {
+        public ControllerTestBase()
+        {
+            Factory = new DefaultWebApplicationFactory();
+        }
+
+        public WebApplicationFactory<Startup> Factory { get; }
+    }
+
+    public class DefaultWebApplicationFactory : WebApplicationFactory<Startup>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+            });
         }
     }
 }
